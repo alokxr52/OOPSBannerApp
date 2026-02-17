22786a6c4c22885e7ad73fb748963be7c7ad0f6a
Your Name <your_email@example.com>
2026-02-17 09:19:08 +0530

    Coded UC1 to Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,14 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application
+ * that displays the Object Oriented Programming System
+ * OOPS acronym to the console.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
@@ -5,3 +16,4 @@ public class OOPSBannerApp {
     }
 
 }
+
