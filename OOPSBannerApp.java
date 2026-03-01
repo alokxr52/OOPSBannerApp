89ebed879a7b3c0da7f33c8431cb3cc3bc6e8afd
Your Name <your_email@example.com>
2026-03-01 14:21:14 +0530

    UC3: Refactor banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,37 @@
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ *
+ * This class improves UC2 by using String.join() instead of
+ * string concatenation to construct each line of the banner.
+ *
+ * @author Alok
+ * @version 3.0
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****    *****    ******    *****  ");
-        System.out.println(" *     *  *     *  *     *  *     * ");
-        System.out.println(" *     *  *     *  *     *  *       ");
-        System.out.println(" *     *  *     *  ******    *****  ");
-        System.out.println(" *     *  *     *  *              * ");
-        System.out.println(" *     *  *     *  *        *     * ");
-        System.out.println("  *****    *****    *         *****  ");
+        System.out.println(String.join(" ",
+                "  *****  ", "  *****  ", " ****** ", "  *****  "));
 
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *     * ", " *     * "));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *     * ", " *       "));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " ******  ", "  *****  "));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *       ", "       * "));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *       ", " *     * "));
+
+        System.out.println(String.join(" ",
+                "  *****  ", "  *****  ", " *       ", "  *****  "));
     }
 }
 
